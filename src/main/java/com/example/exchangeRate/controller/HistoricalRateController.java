ab8bf48983d46aaabd3b836f54c9309542dbb660
ppohlitze <paul.pohlitz@gmx.de>
2019-05-06 23:52:23 +0200

    got rid of the horrible date parameter splitting. luckily spring came in handy

--- src/main/java/com/example/exchangeRate/controller/HistoricalRateController.java
+++ src/main/java/com/example/exchangeRate/controller/HistoricalRateController.java
@@ -3,6 +3,7 @@ package com.example.exchangeRate.controller;
 import com.example.exchangeRate.dtos.Rate;
 import com.example.exchangeRate.services.ExchangeRateRequestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,14 +20,9 @@ public class HistoricalRateController {
 
     @GetMapping("/historical-rate")
     public List<Rate> getHistoricalRate(
-                                    @RequestParam("start-date") String startDate,
-                                    @RequestParam("end-date") String endDate) {
+            @RequestParam("start-date") @DateTimeFormat(iso=DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
+            @RequestParam("end-date") @DateTimeFormat(iso=DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDate) {
 
-        String[] startDates = startDate.split("[^0-9]");
-        String[] endDates = endDate.split("[^0-9]");
-
-        return exchangeRateRequestService.serializeExchangeRates(Optional.of(LocalDateTime.of(
-                Integer.parseInt(startDates[0]), Integer.parseInt(startDates[1]), Integer.parseInt(startDates[2]), Integer.parseInt(startDates[3]), Integer.parseInt(startDates[4]))),
-                Optional.of(LocalDateTime.of(Integer.parseInt(endDates[0]), Integer.parseInt(endDates[1]), Integer.parseInt(endDates[2]), Integer.parseInt(endDates[3]), Integer.parseInt(endDates[4]))));
+        return exchangeRateRequestService.serializeExchangeRates(Optional.of(startDate), Optional.of(endDate));
     }
 }
